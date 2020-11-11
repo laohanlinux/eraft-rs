0a801cf52cdb11b7b498ab1c2980c855c17219d2
zhenghuarong <zhenghuarong@yy.com>
2020-11-12 01:33:05 +0800

    :cloud: :dog:

--- src/paper_test.rs
+++ src/paper_test.rs
@@ -14,11 +14,11 @@
 
 #[cfg(test)]
 mod tests {
-    use crate::raft::{StateType, Raft};
-    use crate::mock::new_test_inner_node;
-    use crate::storage::SafeMemStorage;
-    use crate::raftpb::raft::{Message, HardState};
+    use crate::mock::{new_empty_entry_set, new_entry_set, new_test_inner_node};
+    use crate::raft::{Raft, StateType};
     use crate::raftpb::raft::MessageType::MsgApp;
+    use crate::raftpb::raft::{Entry, HardState, Message};
+    use crate::storage::{SafeMemStorage, Storage};
     use serde::de::Error;
 
     #[test]
@@ -56,18 +56,30 @@ mod tests {
             _ => {}
         }
 
-        raft.step(Message { field_type: MsgApp, term: 2, ..Default::default() });
+        raft.step(Message {
+            field_type: MsgApp,
+            term: 2,
+            ..Default::default()
+        });
 
         assert_eq!(raft.term, 2, "term = {}, want = {}", raft.term, 2);
-        assert_eq!(raft.state, StateType::Follower, "state = {}, want = {}", raft.state, StateType::Follower);
+        assert_eq!(
+            raft.state,
+            StateType::Follower,
+            "state = {}, want = {}",
+            raft.state,
+            StateType::Follower
+        );
     }
 
+    // TODO: FIXME: i don't found more than implementation
     // if a server receives a request with
     // a stale term number, it rejects the request.
     // Our implementation ignores the request instead.
     // Reference: section 5.1
     #[test]
     fn reject_stale_term_message() {
+        flexi_logger::Logger::with_env().start();
         let mut called = false;
         let mut fake_step = |raft: &Raft<SafeMemStorage>, m: Message| -> Result<(), String> {
             called = true;
@@ -75,9 +87,54 @@ mod tests {
         };
 
         let mut raft = new_test_inner_node(0x1, vec![0x1, 0x2, 0x3], 10, 1, SafeMemStorage::new());
-        raft.load_state(&HardState { term: 2, ..Default::default() });
-        raft.step(Message { field_type: MsgApp, term: raft.term - 1, ..Default::default() });
-
+        raft.load_state(&HardState {
+            term: 2,
+            ..Default::default()
+        });
+        let res = raft.step(Message {
+            field_type: MsgApp,
+            term: raft.term - 1,
+            ..Default::default()
+        });
+        info!("{:?}", res);
         assert!(!called, "step_func called = {}, want = {}", called, false);
+    }
+
+    // When server starts up, they begin as followers.
+    // Reference: 5.2
+    #[test]
+    fn start_as_followers() {
+        flexi_logger::Logger::with_env().start();
+        let mut raft = new_test_inner_node(0x1, vec![0x1, 0x2, 0x3], 10, 1, SafeMemStorage::new());
+        assert_eq!(
+            raft.state,
+            StateType::Follower,
+            "state={}, want={}",
+            raft.state,
+            StateType::Follower
+        );
+    }
+
+    // If the leader receives a heartbeat tick,
+    // It will send a `MsgHeartbeat` with m.index = 0, `m.log_term = 0` and empty entries
+    // as heartbeat to all followers.
+    // Reference: 5.2
+    #[test]
+    fn leader_bcast_beat() {
+        // heartbeat interval
+        let hi = 1;
+        let mut raft = new_test_inner_node(0x1, vec![0x1, 0x2, 0x3], 10, 1, SafeMemStorage::new());
+        raft.become_candidate();
+        raft.become_leader();
+
+        for i in 0..10 {
+            let mut entry = new_entry_set(vec![(i + 1, 0)]);
+            must_append_entry(&mut raft, entry);
+        }
+    }
+
+    fn must_append_entry<S: Storage>(raft: &mut Raft<S>, mut ents: Vec<Entry>) {
+        let ok = raft.append_entry(&mut ents);
+        assert!(ok, "entry unexpectedly dropped");
     }
 }
